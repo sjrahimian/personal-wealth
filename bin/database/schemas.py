156838f6fd8409f4433e90d8d1051225f73fad5e
Sama Rahimian <{ID}+{username}@users.noreply.github.com>
2022-07-11 00:16:31 -0400

    Minor changes to the tables: 1. added foreign keys, 2. added checks, 3. columns should have correct types now.

--- bin/database/schemas.py
+++ bin/database/schemas.py
@@ -14,12 +14,11 @@ import sqlite3
 
 class Management():
     Management = """ 
-        CREATE TABLE "Management" (
-            "uwsid"	TEXT NOT NULL UNIQUE,
-            "workspace_name"	TEXT NOT NULL UNIQUE,
-            "first_name"	INTEGER NOT NULL,
+        CREATE TABLE "management" (
+            "workspace_id"	TEXT NOT NULL UNIQUE,
+            "first_name"	TEXT NOT NULL,
             "last_name"	TEXT NOT NULL,
-            PRIMARY KEY("uwsid")
+            PRIMARY KEY("workspace_id")
         ) WITHOUT ROWID
     """
 
@@ -31,27 +30,26 @@ class Management():
 
 class WorkSpace():
     setting = """
-        CREATE TABLE "Setting" (
-            "uwsid"	TEXT NOT NULL UNIQUE,
-            "workspace_name"	TEXT NOT NULL UNIQUE,
+        CREATE TABLE "setting" (
+            "workspace_id"	TEXT NOT NULL UNIQUE,
             "country"	TEXT NOT NULL DEFAULT 'Canada',
             "currency"	TEXT NOT NULL DEFAULT 'CAD',
-            "fiscal_start"	TEXT NOT NULL DEFAULT '01-01',
-            PRIMARY KEY("uwsid")
+            "fiscal_start"	INTEGER NOT NULL DEFAULT '01-01',
+            PRIMARY KEY("workspace_id")
         ) WITHOUT ROWID
     """
 
     income = """
         CREATE TABLE "Income" (
-            "uiid"	INTEGER NOT NULL UNIQUE,
-            "amount"	NUMERIC NOT NULL,
-            "type"	TEXT NOT NULL,
-            "description"	BLOB,
+            "uiid"	TEXT NOT NULL UNIQUE,
+            "amount"	REAL NOT NULL,
+            "type"	TEXT NOT NULL CHECK(type == "active" OR type == "passive" OR type == "portfolio"),
+            "description"	TEXT NOT NULL,
             "tags"	BLOB NOT NULL,
             "category"	BLOB NOT NULL,
             "datetime"	INTEGER NOT NULL,
-            PRIMARY KEY("uiid" AUTOINCREMENT)
-        ) WITHOUT ROWID
+            PRIMARY KEY("uiid")
+        )
     """
 
     all_tables = { setting, income }
