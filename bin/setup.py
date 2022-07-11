7a4debc2a0b50c6850e9af5405cb0dc933fdf8c5
Sama Rahimian <{ID}+{username}@users.noreply.github.com>
2022-07-11 00:21:24 -0400

    Fixed whitespacing.

--- bin/setup.py
+++ bin/setup.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 # setup.py
 
-        """ Setup the databases and ask the basic user account information.
-        
-        When run for the first time, two files should be created: 1 account database and 1 wealth database.
-        Running a 2nd+ time will only create additional wealth databases. Additionally the builder will 
-        contain the setup required for the accounts table (i.e., names)
+    """ Setup the databases and ask the basic user account information.
 
-        Nb. There can be a maximum of 4 wealth databases.
-        """
+    When run for the first time, two files should be created: 1 account database and 1 wealth database.
+    Running a 2nd+ time will only create additional wealth databases. Additionally the builder will 
+    contain the setup required for the accounts table (i.e., names)
 
-        
+    Nb. There can be a maximum of 4 wealth databases.
+    """
+
